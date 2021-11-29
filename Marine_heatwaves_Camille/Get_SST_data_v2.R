a99e5c0112f4071b9c6392292d9134d451305aed
camillemellin <camille.mellin@adelaide.edu.au>
2021-11-29 15:32:47 +1030

    Initial commit and labeller bias analysis

---
+++ Marine_heatwaves_Camille/Get_SST_data_v2.R
@@ -0,0 +1,90 @@
+library(tidyverse)
+library(raster)
+
+site_data <- read_csv(file = "Data/rls_site_ls.csv") 
+
+colSums(is.na(site_data)) # No missing data
+
+# Get the extent of the region for cropping 
+site_data %>% with(c(min(SiteLat),
+                   max(SiteLat),
+                   min(SiteLong),
+                   max(SiteLong)))
+
+ROI <- extent(112 ,157,-25,-8)
+
+# Get the lat and long
+xy <- dplyr::select(site_data, SiteLong, SiteLat)
+
+# Load in the files and crop as we go 
+dhw_data <- crop(stack(x = "Data/ct5km_dhw-max_v3.1_2016.nc", varname = "degree_heating_week"), ROI)
+ssta_data <- crop(stack(x = "Data/ct5km_ssta-max_v3.1_2016.nc", varname = "sea_surface_temperature_anomaly"), ROI)
+
+
+# Check that zeros values are valid (which it says here "it is")
+# ncin <- ncdf4::nc_open("Data/ct5km_dhw-max_v3.1_2016.nc")
+# print(ncin)
+# nc_close(ncin)
+
+# Take a look at the data
+plot(dhw_data)
+plot(ssta_data)
+
+# Save and load in as a tiff
+writeRaster(dhw_data, filename = "Data/cropped_dhw_max_2016.tif", format="GTiff", overwrite = TRUE)
+writeRaster(ssta_data, filename = "Data/cropped_ssta_max_2016.tif", format="GTiff", overwrite = TRUE)
+writeRaster(dhw_data, filename = "Data/cropped_dhw_max_2016.raster", format="raster", overwrite = TRUE)
+writeRaster(ssta_data, filename = "Data/cropped_ssta_max_2016.raster", format="raster", overwrite = TRUE)
+# cropped_dhw<-raster("Data/cropped_dhw_max_2016.tif") 
+# all(as.vector(extract(dhw_data, xy)) - extract(cropped_dhw, xy) < 0.000001) # Proof that they are the same thing
+# plot(cropped_dhw)
+
+# This shows missing values are NA
+extract(dhw_data, data.frame(135, -20))
+
+# Extract the data we need based on lat and long
+site_data$maxDHW <- as.vector(extract(dhw_data, xy))
+site_data$maxSSTA <- as.vector(extract(ssta_data, xy))
+
+# The First time round - whole file is 600mb so good to make this a little smaller
+# climatology_data <- crop(stack(x = "Data/noaa_crw_thermal_history_climatology_v2.1.nc", varname = "clim_monthly"), ROI)
+#writeRaster(climatology_data, filename = "Data/cropped_climatology_data.nc", format="CDF", overwrite = TRUE)
+
+# Load the cropped data back in
+climatology_data <- stack("Data/cropped_climatology_data.nc")
+# all(extract(look, xy) - extract(climatology_data, xy) < 0.00000001)
+plot(climatology_data)
+# plot(look)
+
+# Proves that NAs were zero as it is on the Australian continent
+extract(climatology_data, data.frame(135, -20))
+
+# Replace zeros with NA
+climatology_data[climatology_data == 0] <- NA
+
+# Get a matrix with one col for each month
+climatology_extract <- extract(climatology_data, xy)
+
+# Which points are we missing
+missing_points <- which(is.na(climatology_extract[,1]))
+
+# Just one layer
+y<- climatology_data[[1]]
+  
+# Get missing points
+for (i in missing_points){
+  
+  # Find nearest value
+  index_value <- which.min(distanceFromPoints(y, xy[i,]) + y) # Note adding na will return only valid SSTs
+  # Extract values
+  climatology_extract[i,] <- climatology_data[index_value]
+  
+}
+  
+# Append some more appropriate column names
+colnames(climatology_extract) <- paste(month.abb, "SST_cl", sep = "_")
+
+site_data <- cbind(site_data, climatology_extract)
+
+# Save
+write.csv(site_data, file = "Data/sites_with_sst.csv", row.names = FALSE)
---
+++ NESP-coral-analysis-revised.R
@@ -0,0 +1,175 @@
+
+##############################################################
+# NESP CORAL ANALYSIS - REVISED - CM 29/11/21                #
+##############################################################
+
+# Load libraries ------------
+rm(list = ls())
+
+library(RLSMetrics)
+library(RColorBrewer)
+library(PBSmapping)
+library(goeveg)
+library(ggplot2)
+library(ggplotify)
+library(gridExtra)
+library(tidyverse)
+library(vegan)
+library(betapart)
+library(labdsv)
+library(mvpart)
+library(adespatial)
+library(psych)
+library(gbm)
+library(pdist)
+library(ggpubr)
+library(sp)
+library(sf)
+library(tmap)
+library(raster)
+library(dplyr)
+library(tidyverse)
+
+# Functions ----------------
+
+addalpha <- function(colors, alpha=1.0) {
+  r <- col2rgb(colors, alpha=T)
+  # Apply alpha
+  r[4,] <- alpha*255
+  r <- r/255.0
+  return(rgb(r[1,], r[2,], r[3,], r[4,]))
+}
+
+summary_indval <- function (object, p = 0.05) 
+{   tmp <- data.frame(object$maxcls[object$pval <= p], round(object$indcls[object$pval <= p], 3), object$pval[object$pval <= p])
+names(tmp) <- c("cluster", "indicator_value", "probability")
+data.frame(tmp[order(tmp$cluster, -tmp$indicator_value),])
+}
+
+# Load and filter benthic data --------------
+
+setwd("/Users/Camille/Dropbox/My documents/Projects/Future Fellowship/RESEARCH/NESP-coral-analysis-revised")
+#load("Coral_indicators.RData")
+source("brt.functions.R")
+
+dhw.raster <- raster::raster("Marine_heatwaves_Camille/Data/cropped_dhw_max_2016.grd")
+ssta.raster <- raster::raster("Marine_heatwaves_Camille/Data/cropped_ssta_max_2016.grd")
+
+#rls.sites.unique <- read.csv("rls.sites.unique.csv")
+
+#Ningaloo_NW_GBR_SurveyList <- read.csv("Ningaloo_NW_GBR_SurveyList.csv")
+#Ningaloo_NW_GBR_SurveyList$SurveyID <- factor(Ningaloo_NW_GBR_SurveyList$SurveyID)
+
+bent.dat <- read.csv("NESP-Extract_2021-11-09.csv")
+
+bent.dat$survey_date <- as.Date(bent.dat$survey_date, tryFormats = "%d/%m/%y")
+bent.dat$survey_year <- format(bent.dat$survey_date, '%Y')
+
+
+bent.dat$survey_id <- factor(bent.dat$survey_id)
+bent.dat$dataset_id <- factor(bent.dat$dataset_id)
+
+
+# Remove duplicates
+dataset_id_duplicates <- c("912346798_RLS Australian Coral Species List_982", 
+                           "912351889_RLS Catalogue_946", 
+                           "912352031_RLS Catalogue_946",
+                           "912352656_RLS Catalogue_939",
+                           "912352687_RLS Catalogue_928")
+
+bent.dat <- bent.dat %>% filter(!dataset_id %in% dataset_id_duplicates)
+
+# Add 'unfinished' missing info
+bent.dat$is_finished[!(bent.dat$is_finished %in% c('yes', 'no'))] <- 'no'
+
+
+# Metadata --------------
+
+# Check unique survey_id/dataset_id match (i.e. each transect is scored only once)
+bent.meta <- bent.dat %>% group_by(survey_id, dataset_id) %>% summarize() %>%
+                          group_by(survey_id) %>% summarize(count = n_distinct(dataset_id))
+
+# Check number of finishes vs unfinished surveys
+bent.meta <- bent.dat %>% group_by(survey_id, is_finished, is.infilled.in.previous.extract) %>% summarize()
+
+
+# Check labeller bias in non-coral benthic scores --------------
+
+bent.dat.check <- bent.dat %>% dplyr::select(survey_id, location, site_code, site_name, latitude, longitude, survey_date, survey_year,
+                                             annotation_labeller_username, label, flag_dead, flag_bleached, RLS_category, RLE_category,
+                                             percent_cover, is_finished, is_NESP_pre.post, is.infilled.in.previous.extract) %>%
+                                filter(is_finished == "yes" & RLE_category %in% c("0", "Kelp"))
+
+# Check number of labellers per survey_id
+bent.dat.check2 <- bent.dat.check %>% group_by(survey_id, annotation_labeller_username) %>% summarize() %>%
+                                      group_by(survey_id) %>% summarize(n_labellers = n_distinct(annotation_labeller_username))
+
+# Wide format for single-labeller surveys only
+bent.dat.check.wide <- bent.dat.check %>% filter(survey_id %in% bent.dat.check2$survey_id[bent.dat.check2$n_labellers == 1]) %>%
+                        group_by(survey_id, location, survey_year, is_NESP_pre.post, labeller = annotation_labeller_username, label) %>%
+                        summarize(percent_cover = sum(percent_cover)) %>%
+                        pivot_wider(names_from = label, values_from = percent_cover, values_fill = 0) %>% data.frame
+
+# Divide by total cover of non-coral categories
+bent.dat.check.wide$total <- rowSums(base::subset(bent.dat.check.wide, select = Bare.Rock:Colonial.Anemones..Zoanthids.and.Corallimorphs))
+
+bent.dat.check.wide[, 6:44] <- bent.dat.check.wide[, 6:44]/bent.dat.check.wide$total
+
+bent.dat.check.wide$total <- rowSums(base::subset(bent.dat.check.wide, select = Bare.Rock:Colonial.Anemones..Zoanthids.and.Corallimorphs))
+
+bent.dat.check.wide <- bent.dat.check.wide %>% filter(!is.na(total))
+
+
+# CAPSCALE testing for labeller effect
+
+capscale.dat <- sqrt(subset(bent.dat.check.wide, select = Bare.Rock:Colonial.Anemones..Zoanthids.and.Corallimorphs))
+
+capscale <- capscale(capscale.dat ~ labeller + location + survey_year + is_NESP_pre.post, data = bent.dat.check.wide, distance = "bray", add = T)
+
+capscale
+plot(capscale)
+anova(capscale, permutations = how(nperm=  999), by = "term")
+
+boxplot(Turfing.algae...2.cm.high.algal.sediment.mat.on.rock. ~ labeller, data = bent.dat.check.wide)
+
+# Check which benthic categories are sensitive to labeller effect: Kruskal-Wallis test + boxplot
+kw.pvalues <- data.frame(label = names(bent.dat.check.wide)[6:44], kw.pvalues = NA)
+
+for (i in 6:44) {
+  kw.test <- kruskal.test(bent.dat.check.wide[,i] ~ bent.dat.check.wide$labeller)
+  kw.pvalues$kw.pvalues[i-5] <- ifelse(kw.test$p.value < 0.001, 0, 0.05)
+}
+
+par(mfcol = c(5,2), mai = c(.3,.6,.2,.2))
+boxplot(Bare.Rock ~ labeller, data = bent.dat.check.wide)
+abline(h = mean(bent.dat.check.wide$Bare.Rock), col = "red")
+
+boxplot(Coral.rubble ~ labeller, data = bent.dat.check.wide)
+abline(h = mean(bent.dat.check.wide$Coral.rubble), col = "red")
+
+boxplot(Coral.rubble.with.turf.encrusting.algae ~ labeller, data = bent.dat.check.wide)
+abline(h = mean(bent.dat.check.wide$Coral.rubble.with.turf.encrusting.algae), col = "red")
+
+boxplot(Turfing.algae...2.cm.high.algal.sediment.mat.on.rock. ~ labeller, data = bent.dat.check.wide)
+abline(h = mean(bent.dat.check.wide$Turfing.algae...2.cm.high.algal.sediment.mat.on.rock.), col = "red")
+
+boxplot(Crustose.coralline.algae ~ labeller, data = bent.dat.check.wide)
+abline(h = mean(bent.dat.check.wide$Crustose.coralline.algae), col = "red")
+
+boxplot(Sponges..encrusting. ~ labeller, data = bent.dat.check.wide)
+abline(h = mean(bent.dat.check.wide$Sponges..encrusting.), col = "red")
+
+boxplot(Encrusting.leathery.algae ~ labeller, data = bent.dat.check.wide)
+abline(h = mean(bent.dat.check.wide$Encrusting.leathery.algae), col = "red")
+
+boxplot(Geniculate.coralline.algae ~ labeller, data = bent.dat.check.wide)
+abline(h = mean(bent.dat.check.wide$Geniculate.coralline.algae), col = "red")
+
+boxplot(Slime..not.trapping.sediment. ~ labeller, data = bent.dat.check.wide)
+abline(h = mean(bent.dat.check.wide$Slime..not.trapping.sediment.), col = "red")
+
+boxplot(Filamentous.brown.algae_epiphyte ~ labeller, data = bent.dat.check.wide)
+abline(h = mean(bent.dat.check.wide$Filamentous.brown.algae_epiphyte), col = "red")
+
+
+
